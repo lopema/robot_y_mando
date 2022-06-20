f9bd4f3a50942d18a894f829228db6cca5813ad5
lopema <47774854+lopema@users.noreply.github.com>
2022-06-20 13:46:10 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,8 +16,22 @@ maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         radio.sendString("IZQ")
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . . . .
+            `)
     }
     if (input.buttonIsPressed(Button.B)) {
         radio.sendString("DRCHA")
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . . . .
+            `)
     }
 })
